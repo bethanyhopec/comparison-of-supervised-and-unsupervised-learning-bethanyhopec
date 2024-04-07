933068cfd0e49749ca9120b723728358ef249ed8
Bethany Hope Cabristante <99635993+bethanyhopec@users.noreply.github.com>
2024-04-07 19:23:48 +0800

    Update 2Unsupervised Learning.py

--- pages/2Unsupervised Learning.py
+++ pages/2Unsupervised Learning.py
@@ -41,7 +41,6 @@ def app():
     if st.button("Begin"):
         # Load the Iris dataset
         penguins = pd.read_csv('penguins.csv', header=None)
-        penguins = datasets.load_penguins()
         X = penguins.data  # Features
         y = penguins.target  # Target labels (species)
 
